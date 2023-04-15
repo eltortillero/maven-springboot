72f33d7f875e490fdf96cbdfa2db30aee55d724f
Horeb Gastelum <horeb@live.com.mx>
2023-04-15 14:53:03 -0700

    [java spring boot maven server app]

---
+++ src/main/java/com/mavenspringboot/mavenspringbootserver/MavenSpringbootServerApplication.java
@@ -0,0 +1,13 @@
+package com.mavenspringboot.mavenspringbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringbootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringbootServerApplication.class, args);
+	}
+
+}
