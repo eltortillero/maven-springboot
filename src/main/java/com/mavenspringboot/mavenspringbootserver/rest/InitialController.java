2ddee12d068643dbf26df470e74bf6f2dc67b69b
Horeb Gastelum <horeb@live.com.mx>
2023-04-15 17:50:49 -0700

    spring boot actuator config for devops support exposure

--- src/main/java/com/mavenspringboot/mavenspringbootserver/rest/InitialController.java
+++ src/main/java/com/mavenspringboot/mavenspringbootserver/rest/InitialController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class InitialController {
     @GetMapping("/")
     public String handShake() {
-        return "<h1>Handshake was successful<h1>";
+        return "<h1>Handshake was successful again!!!<h1>";
+    }
+    @GetMapping("/endpoint-test")
+    public String testEndpoint() {
+        return "This is a test string for endpoint-test";
     }
 }
