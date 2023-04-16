3cb217b54703c5559d7032bd28ed75cbf366a485
Horeb Gastelum <horeb@live.com.mx>
2023-04-15 21:21:42 -0700

    application.properties injection access

--- src/main/java/com/mavenspringboot/mavenspringbootserver/rest/InitialController.java
+++ src/main/java/com/mavenspringboot/mavenspringbootserver/rest/InitialController.java
@@ -1,16 +1,19 @@
 package com.mavenspringboot.mavenspringbootserver.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InitialController {
+    @Value("${application.version}")
+    private String version;
     @GetMapping("/")
     public String handShake() {
         return "<h1>Handshake was successful again!!!<h1>";
     }
     @GetMapping("/endpoint-test")
     public String testEndpoint() {
-        return "This is a test string for endpoint-test";
+        return this.version;
     }
 }
