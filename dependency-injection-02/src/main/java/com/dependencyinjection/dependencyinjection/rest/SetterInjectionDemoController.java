9ccaa88a7d3e09bc2f4dec3c71f801b6f289ea85
Horeb Gastelum <horeb@live.com.mx>
2023-04-22 11:08:10 -0700

    [optional injection]

---
+++ dependency-injection-02/src/main/java/com/dependencyinjection/dependencyinjection/rest/SetterInjectionDemoController.java
@@ -0,0 +1,20 @@
+package com.dependencyinjection.dependencyinjection.rest;
+
+import com.dependencyinjection.dependencyinjection.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SetterInjectionDemoController {
+    private Coach _optionalCoach;
+    @Autowired
+    public void setOptionalCoach(Coach optionalCoach) {
+        _optionalCoach = optionalCoach;
+    }
+    @GetMapping("/optional-coach")
+    public String setterInjectionDemoEndpoint() {
+        if (_optionalCoach == null) return "banana";
+        return _optionalCoach.getDailyWorkout();
+    }
+}
