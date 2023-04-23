6daec985336e9787f482597b353cb173cf454e66
Horeb Gastelum <horeb@live.com.mx>
2023-04-23 12:11:23 -0700

    [finally rest app]

---
+++ rest-api/src/test/java/com/bananastudios/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bananastudios.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
